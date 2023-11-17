3ce3397574f4d71467fd05d9d22a6e91998abbdd
joonjae_cnea <leecartas@hotmail.com>
2023-11-17 09:38:13 -0300

    Insert *.svh

--- 05_UVM_for_Verification_part2_projects/04_uart/agents/uart_agent/uart_agent_pkg.sv
+++ 05_UVM_for_Verification_part2_projects/04_uart/agents/uart_agent/uart_agent_pkg.sv
@@ -1,7 +1,7 @@
 //  Package: uart_agent_pkg
 //
 package uart_agent_pkg;
-    
+
     import uvm_pkg::*;
     `include "uvm_macros.svh"
 
@@ -9,8 +9,13 @@ package uart_agent_pkg;
     typedef enum bit [1:0] {reset_asserted = 0, random_baud = 1} oper_mode;
 
     //  Group: Parameters
-    
-    `include "uart_seq_item.svh"
     `include "uart_agent.svh"
-    
+    `include "uart_baud_seq.svh"
+    `include "uart_driver.svh"
+    `include "uart_env.svh"
+    `include "uart_monitor.svh"
+    `include "uart_reset_seq.svh"
+    `include "uart_sco.svh"
+    `include "uart_seq_item.svh"
+
 endpackage: uart_agent_pkg
